55b8cd5e0e496f5a9eea556d9d478fee3bcf338d
antonyxiao <44051705+antonyxiao@users.noreply.github.com>
2024-09-06 17:58:35 -0700

    Add files via upload
    update, with led now

--- rules.mk
+++ rules.mk
@@ -2,6 +2,6 @@ BOOTLOADER = atmel-dfu
 DYNAMIC_MACRO_ENABLE = yes
 MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = no
+OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
