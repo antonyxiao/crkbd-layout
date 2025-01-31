2ea263617af959bffd2f420e9f0cb4e849cbe416
Antony Xiao <antony.xiao@acdsee.com>
2025-01-31 11:54:32 -0800

    update

--- keymap.c
+++ keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX,  KC_EQL, KC_LPRN, KC_LCBR, KC_LABK,                      KC_RABK, KC_RCBR, KC_RPRN, DM_PLY1, KC_TILD, KC_GRV,
+      KC_LCTL,S(KC_QUOT),KC_EQL, KC_LPRN, KC_LCBR, KC_LABK,                      KC_RABK, KC_RCBR, KC_RPRN, DM_PLY1, KC_TILD, KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, KC_TILD, XXXXXXX, KC_MINS, KC_BSLS, KC_LBRC,                      KC_RBRC, KC_UNDS, KC_PLUS, DM_PLY2, KC_BSLS, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
