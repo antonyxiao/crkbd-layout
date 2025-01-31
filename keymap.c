802b4d113d29433058867ec4e4053ecafe8c483a
Antony Xiao <antonyxiao16@gmail.com>
2025-01-31 12:09:20 -0800

    update

--- keymap.c
+++ keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                    KC_LGUI,LT(_LOWER,KC_SPC),KC_SPC, RSFT_T(KC_ENT),MO(_RAISE), KC_RALT
+                                        KC_LGUI, MO(_LOWER), KC_SPC,  RSFT_T(KC_ENT),MO(_RAISE), KC_RALT
                                       //`--------------------------'  `--------------------------'
 
   ),
--- rules.mk
+++ rules.mk
@@ -1,7 +1,8 @@
 BOOTLOADER = atmel-dfu
 DYNAMIC_MACRO_ENABLE = yes
 MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+# RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no
 OLED_ENABLE     = yes
 # OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
